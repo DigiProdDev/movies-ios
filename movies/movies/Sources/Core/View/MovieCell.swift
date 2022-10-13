88effe2df4cd51fd8bbe105808570b1f576ae5b0
acc-joao-padilha <joao.gabriel.padilha@accenture.com>
2022-10-13 14:37:37 -0300

    Update MovieCell.swift

--- movies/movies/Sources/Core/View/MovieCell.swift
+++ movies/movies/Sources/Core/View/MovieCell.swift
@@ -53,6 +53,7 @@ final class MovieCell: UICollectionViewCell {
         setupView()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
