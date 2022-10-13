c7983371769318a92bb21941eb24001e6b96c98d
Gustavo Garcia Leite <gustavo.garcia.leite@accenture.com>
2022-10-13 12:56:52 -0300

    Fix code review adjustments

--- movies/setup.sh
+++ movies/setup.sh
@@ -1,9 +1,16 @@
+install_homebrew() {
+    echo "Installing homebrew";
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo "Homebrew installed";
+}
+
 install_brewers() {
-    echo "Installing..."
+    echo "Installing brewers..."
     brew install swiftlint
     echo "brewers installed"
 }
 
+install_homebrew
 install_brewers
 
 echo "You're all set! It's time to start coding! 👨‍💻👩‍💻"
